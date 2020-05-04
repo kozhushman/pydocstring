0d8c7784de0524304c1b4e97dcc172025e35e9ef
nikkoz <nikita.kozhushko@kronshtadt.ru>
2020-05-04 17:23:57 +0300

    removed hello world

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.0.4] - 04.05.2020
 ### Fixed
 - Bug that causes command not working
+
+## [0.0.5] - 04.05.2020
+### Fixed
+- Removed hello world message
--- src/extension.ts
+++ src/extension.ts
@@ -182,10 +182,6 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 		}
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from python_docstring!');
 	});
 
 	context.subscriptions.push(disposable);
